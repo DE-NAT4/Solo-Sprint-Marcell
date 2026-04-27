10c57361c1cfca752e28b933292ac34980495f59
kushima028 <1312011@genstudents.org>
2026-04-27 11:36:42 +0100

    add add_user function

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import csv
 users_active = [{'name': 'John', 'password': '1234' }, {'name': 'Kate', 'password': '5678'}]
 users_disabled = []
 
+def save_users_to_csv():
+    pass
+
+
+def load_users_from_csv():
+    pass
+
+
+
 def print_menu():
     print("\n-----------------------")
     print("User Management System\n")
@@ -28,6 +37,30 @@ def print_users():
     for i, user in enumerate(users_disabled, start=1):
         print(f"{i}. Name: {user['name']} | Password: {user['password']}")   
 
+def add_user():
+    while True:
+        username = input("Enter a username: ")
+        password = input("Enter a password: ")
+        status = input('Enter status ("active" or "disabled"): ').lower()
+
+        match status:
+            case "active":
+                users_active.append({
+                    'name': username,
+                    'password': password,
+                    'status': status
+                })
+                break
+            case "disabled":
+                users_disabled.append({
+                    'name': username,
+                    'password': password,
+                    'status': status
+                })
+                break
+            case _:
+                print("Invalis status")
+
 
 while True:
     print_menu()        
@@ -37,7 +70,8 @@ while True:
         case "0":
             break
         case "1":
-            print("You're in option 1")
+            print("\n---Add User")
+            add_user()
         case "2":
             print("\n---View Users---")
             print_users()
