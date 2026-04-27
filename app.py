b6c89308b0fc31dc949ced92e0b5a68e5a6ccbe6
kushima028 <1312011@genstudents.org>
2026-04-27 12:30:02 +0100

    add save_to_csv and toggle_status

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import csv
 
-users_active = [{'name': 'John', 'password': '1234' }, {'name': 'Kate', 'password': '5678'}]
+users_active = [{'name': 'John', 'password': '1234', 'status': 'active'}, {'name': 'Kate', 'password': '5678', 'status': 'active'}]
 users_disabled = []
 
 def save_users_to_csv():
-    pass
+    with open('users.csv', 'w', newline='') as csvfile:
+        fieldnames = ['username', 'password', 'status']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+        writer.writeheader()
+        for user in users_active:
+            writer.writerow({'username': user['name'], 'password': user['password'], 'status': user['status']})
+
+        for user in users_disabled:
+            writer.writerow({'username': user['name'], 'password': user['password'], 'status': user['status']})
 
 def load_users_from_csv():
     pass
@@ -59,7 +67,58 @@ def add_user():
                 })
                 break
             case _:
-                print("Invalis status")
+                print("Invalid status")
+
+
+def toggle_user_status():
+    while True:
+        choice = input('Type "disable" to disable an active user or type "enable" to enable a  disabled user: ').lower()
+
+        match choice:
+            case "disable":
+                if not users_active:
+                    print("No active users")
+
+                for i, user in enumerate(users_active, start=0):
+                    print(f"Index: {i} | Name: {user['name']}")
+
+                index = int(input("Enter the index of the user you want to disable: "))
+                try: 
+                    user = users_active[index]
+                    user['status'] = 'disabled'
+                    users_disabled.append(user)
+
+                    users_active.pop(index)
+                    
+                    break
+
+                except Exception as e:
+                    print(f"Invalid input. Error: {e} ")
+
+            case "enable":
+                if not users_disabled:
+                    print("No active users")
+
+                for i, user in enumerate(users_disabled, start=0):
+                    print(f"Index: {i} | Name: {user['name']}")
+
+                index = int(input("Enter the index of the user you want to enable: "))
+                try: 
+                    user = users_disabled[index]
+                    user['status'] = 'enabled'
+                    users_active.append(user)
+
+                    users_disabled.pop(index)
+                    
+                    break
+
+                except Exception as e:
+                    print(f"Invalid input. Error: {e} ")
+
+            case _:
+                print("Invalid input")
+
+            
 
 
 while True:
@@ -68,6 +127,7 @@ while True:
     
     match menu_selection:
         case "0":
+            save_users_to_csv()
             break
         case "1":
             print("\n---Add User")
@@ -76,7 +136,8 @@ while True:
             print("\n---View Users---")
             print_users()
         case "3":
-            print("You're in option 3")
+            print("\n---Enable/Disable User---")
+            toggle_user_status()
         case _:
             print("Invalid option, try again")
         
