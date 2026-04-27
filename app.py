9a0a14dcd2a8a8b600160a885ea4b0bcac297035
kushima028 <1312011@genstudents.org>
2026-04-27 10:58:19 +0100

    add view users function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import csv
 
-users_active = []
+users_active = [{'name': 'John', 'password': '1234' }, {'name': 'Kate', 'password': '5678'}]
 users_disabled = []
 
 def print_menu():
@@ -13,17 +13,34 @@ def print_menu():
     print("-----------------------")
 
 
+def print_users():
+    print("Active users:")
+    if not users_active:
+        print("No Users")
+
+    for i, user in enumerate(users_active, start=1):
+        print(f"{i}. Name: {user['name']} | Password: {user['password']}")
+    
+    print("\nDisabled users:")
+    if not users_disabled:
+        print("No Users")
+
+    for i, user in enumerate(users_disabled, start=1):
+        print(f"{i}. Name: {user['name']} | Password: {user['password']}")   
+
+
 while True:
     print_menu()        
     menu_selection = input("Chose a menu option: ")
-
+    
     match menu_selection:
         case "0":
             break
         case "1":
             print("You're in option 1")
         case "2":
-            print("You're in option 2")
+            print("\n---View Users---")
+            print_users()
         case "3":
             print("You're in option 3")
         case _:
